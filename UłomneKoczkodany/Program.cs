847fb452fe101f990edf12a5a3b2c18f072088c5
TheFatKidfromZamosc <witiuk61@gmail.com>
2025-05-21 09:54:36 +0200

    Welcome to the world of the plastic beach

---
+++ UłomneKoczkodany/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace UłomneKoczkodany
+{
+    internal class Program
+    {
+        public class Bank
+
+        {
+            private double stanKonta;
+            public int nrKlienta { get; private set; }
+            public Bank(int nrKlienta, double stanKonta = 0)
+            {
+                this.nrKlienta = nrKlienta;
+                this.stanKonta = stanKonta;
+            }
+            public double getStanKonta() => stanKonta;
+            public bool wplata(double kwota)
+            {
+                if (kwota > 0)
+                {
+                    stanKonta += kwota;
+                    return true;
+                }
+                return false;
+            }
+            public bool wyplata(double kwota)
+            {
+                if (kwota <=0)
+                    return false;
+                if (stanKonta > kwota)
+                {
+                    stanKonta -= kwota;
+                    return true;
+                }
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var cos = new Bank(1000);
+            cos.wplata(100000);
+            Console.WriteLine(cos.getStanKonta());
+            cos.wyplata(1000);
+            Console.WriteLine(cos.getStanKonta());
+        }
+    }
+}
---
+++ UłomneKoczkodany/ProgramTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static UłomneKoczkodany.Program;
+
+
+namespace UłomneKoczkodany
+{
+    internal class BankTesting
+    {
+    [TestClass]
+        public class BankTest
+        {
+            [TestMethod]
+            public void WplataRetrurnCorrectResult()
+            {
+                var ban = new Bank(0, 0);
+                bool result = ban.wplata(1000);
+                ban.Equals(true);
+                double kwota = ban.getStanKonta();
+                kwota.Equals(1000);
+            }
+             [TestMethod]
+        public void Wplata_RetrurnIncorrectResult()
+            {
+                var ban = new Bank(0, 0);
+                bool result = ban.wplata(1000);
+                ban.Equals(false);
+                double kwota = ban.getStanKonta();
+                kwota.Equals(99999999);
+            }
+            [TestMethod]
+            public void Wyplata_ReturnCorrectResult()
+            {
+                var ban = new Bank(1000);
+                var result = ban.wyplata(1);
+                ban.Equals(true);
+                var kwota = ban.getStanKonta();
+                kwota.Equals(999);
+            }
+            [TestMethod]
+            public void Wyplata_ReturnIncorrectResult()
+            {
+                var ban = new Bank(1000);
+                var result = ban.wyplata(1);
+                ban.Equals(false);
+                var kwota = ban.getStanKonta();
+                kwota.Equals(999999);
+            }
+        }
+
+
+    }
+}
